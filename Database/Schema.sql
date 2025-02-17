fd49b84d9cb82b5f26353300105f9514c21afcfb
JRay350 <121981733+JRay350@users.noreply.github.com>
2025-02-17 13:55:31 -0800

    Add files via upload

---
+++ Database/Schema.sql
@@ -0,0 +1,79 @@
+--
+-- File generated with SQLiteStudio v3.4.15 on Mon Feb 17 13:55:05 2025
+--
+-- Text encoding used: System
+--
+PRAGMA foreign_keys = off;
+BEGIN TRANSACTION;
+
+-- Table: Category
+DROP TABLE IF EXISTS Category;
+
+CREATE TABLE IF NOT EXISTS Category (
+    CategoryID INTEGER PRIMARY KEY AUTOINCREMENT,
+    Name       TEXT    NOT NULL,
+    ItemID     INTEGER REFERENCES Item (ItemID) 
+)
+STRICT;
+
+
+-- Table: Item
+DROP TABLE IF EXISTS Item;
+
+CREATE TABLE IF NOT EXISTS Item (
+    ItemID          INTEGER PRIMARY KEY,
+    ItemName        TEXT    NOT NULL,
+    Quantity        INTEGER NOT NULL,
+    Price           NUMERIC NOT NULL,
+    PurchaseDate    TEXT    NOT NULL,
+    ExpirationDate  TEXT,
+    StorageLocation TEXT,
+    UserID          INTEGER REFERENCES User (UserID),
+    FOREIGN KEY (
+        UserID
+    )
+    REFERENCES User (UserID) 
+);
+
+
+-- Table: PriceHistory
+DROP TABLE IF EXISTS PriceHistory;
+
+CREATE TABLE IF NOT EXISTS PriceHistory (
+    PriceID      INTEGER PRIMARY KEY,
+    DateRecorded TEXT    NOT NULL,
+    Price        NUMERIC NOT NULL,
+    ItemID       INTEGER,
+    FOREIGN KEY (
+        ItemID
+    )
+    REFERENCES Item (ItemID) 
+);
+
+
+-- Table: Usage
+DROP TABLE IF EXISTS Usage;
+
+CREATE TABLE IF NOT EXISTS Usage (
+    UsageID      INTEGER PRIMARY KEY,
+    DateUsed     TEXT    NOT NULL,
+    QuantityUsed INTEGER NOT NULL,
+    ItemID       INTEGER,
+    FOREIGN KEY (
+        ItemID
+    )
+    REFERENCES Item (ItemID) 
+);
+
+
+-- Table: User
+DROP TABLE IF EXISTS User;
+
+CREATE TABLE IF NOT EXISTS User (
+    UserID INTEGER PRIMARY KEY,
+    Name   TEXT    NOT NULL
+);
+
+
+COMMIT TRANSACTION;
+PRAGMA foreign_keys = on;
